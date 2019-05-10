7fb3ecfbe6c73023848492fd7f5a89445eb68ae5
senoopsy <48882245+senoopsy@users.noreply.github.com>
2019-05-10 19:50:32 +0530

    Update #12 user input.py

--- #12 user input.py
+++ #12 user input.py
@@ -1,8 +1,22 @@
 x = int(input('Enter First Number'))
 y = int(input('Enter Second Number'))
+z = x / y
+print(z)
+
+x = int(input('Enter 1st Number'))
+y = int(input('Enter 2nd Number'))
+z = x * y
+print(z)
+
+x = int(input('Enter 1st Number'))
+y = int(input('Enter 2nd Number'))
 z = x + y
 print(z)
 
+x = int(input('Enter 1st Number'))
+y = int(input('Enter 2nd Number'))
+z = x - y
+print(z)
 
 ch = input('Enter a Character')
 print(ch[0:3])
